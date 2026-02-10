e51c1534a004e33ed41a919b115af48bfd135d94
mav <sadness110as@gmail.com>
2026-02-10 17:27:07 -0500

    add EJS

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const app = express();
 /*app.get("/", (req, res) => {
   res.send("Hola mundo 🌎");
 });*/
+app.set("view engine","ejs");
+
+// estaticos
 app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+	res.render("index",{
+		titulo:"WebSite with NodeJS",
+		mensaje:"Hi world from Express and EJS 🚀"
+	});
+});
 
 const puerto = process.env.PORT || 3000;
 app.listen(puerto, () => {
