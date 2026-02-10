cfc742a14dd96b9107c6756597878b3dd442bc53
mav <sadness110as@gmail.com>
2026-02-10 16:37:59 -0500

    add page html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hola mundo 🌎");
-});
+});*/
+app.use(express.static("public"));
+
 const puerto = process.env.PORT || 3000;
 app.listen(puerto, () => {
   console.log("Servidor corriendo en el puerto",puerto);
